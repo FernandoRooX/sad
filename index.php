ee6857bd15c67f00844aff6bc242bd0dc14c278a
Reuel <reuel_sc@live.com>
2014-11-21 16:31:19 -0200

    removido rodape statico e incluido rodape dinâmico

--- index.php
+++ index.php
@@ -111,27 +111,7 @@ endif;
         </div>
     </div>
 
-    <footer class="navbar navbar-inverse navbar-fixed-bottom">
-		<div class="copyright">
-			<div class="container container-fluid">
-			  <div class="row">
-				<div class="col-md-12">
-				  <p>&copy; Todos os direitos reservados</p>
-				</div>
-			  </div>
-			</div>
-		</div>
-	<footer class="navbar navbar-inverse navbar-fixed-bottom">
-		<div class="copyright">
-			<div class="container container-fluid">
-			  <div class="row">
-				<div class="col-md-12">
-				  <p>&copy; Todos os direitos reservados</p>
-				</div>
-			  </div>
-			</div>
-		</div>
-	</footer>
+    <?php require_once './inc/rodape.php'; ?>
 	
 	<script src="../js/jquery.min.js"></script>
 	<script src="../js/bootstrap.min.js"></script>
