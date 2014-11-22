048522a582e85cabf79242d178cd44891ed2a28e
Reuel <reuel_sc@live.com>
2014-11-22 19:57:31 -0200

    ajustando rodapé

--- professor/turmas.php
+++ professor/turmas.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <?php 
-    
+    session_start();
     $variavel = "professor";
     require_once '../classes/validaAcesso.php';
-    session_start(); 
     ?>
     <head>
         <?php require_once '../inc/head.php'; ?>
@@ -18,9 +17,8 @@
                 <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
         <![endif]-->
     </head>
-    <body>
-<?php require_once '../topo.php'; ?>
-
+<body>
+    <?php require_once '../topo.php'; ?>
         <div class="wrapper" role="main">
             <div class="container-fluid container">
                 <div class="row">
@@ -54,25 +52,30 @@
                                     </tr>
                                 </thead>
                                 <tbody class="h5">
-                                    <?php       $rsUmProf = @mysql_query("select pessoa.*, professor.* FROM professor inner join pessoa on pessoa.idPESSOA = professor.PESSOA_idPESSOA WHERE professor.PESSOA_idPESSOA = '$idPessoa'");
-                                                $objUmProf = @mysql_fetch_object($rsUmProf);
-                                                echo $objUmProf->cod;
-                                                $rs = @mysql_query("select turma.*, curso.* FROM turma inner join curso on turma.CURSO_cod = curso.cod WHERE turma.PROFESSOR_cod = $objUmProf->cod");
-                                                while ($obj = @mysql_fetch_object($rs)) {
-                                                    
-                                                ?>
+                                    <?php
+                                        $rsUmProf = @mysql_query("select pessoa.*, professor.* FROM professor inner join pessoa on pessoa.idPESSOA = professor.PESSOA_idPESSOA WHERE professor.PESSOA_idPESSOA = '$idPessoa'");
+                                        $objUmProf = @mysql_fetch_object($rsUmProf);
+                                        echo $objUmProf->cod;
+                                        $rs = @mysql_query("select turma.*, curso.* FROM turma inner join curso on turma.CURSO_cod = curso.cod WHERE turma.PROFESSOR_cod = $objUmProf->cod");
+                                        while ($obj = @mysql_fetch_object($rs)) {
+                                    ?>
                                     <tr>
-                                        <td><?php echo("<a href='perfilTurma.php?idTurma=" . $obj->turma_cod . "' > " . $obj->periodo . "/" . $obj->ano . " - " . $obj->turno . " - " . $obj->nome." - " . $obj->modalidade." </a>");
-                                                ?></td>
-                                        <td><?php $codigo = $obj->PROFESSOR_cod;
-                                       $rsCodPessoa = mysql_query("SELECT PESSOA_idPESSOA FROM professor WHERE cod=$codigo");
-                                       $objCodPessoa = mysql_fetch_object($rsCodPessoa);
-                                       $codPessoa = $objCodPessoa->PESSOA_idPESSOA;
-                                        $rsProf = mysql_query("select professor.*, pessoa.* FROM professor inner join pessoa on $codPessoa= pessoa.idPESSOA ORDER BY nome");
-                                                  $objProf = mysql_fetch_object($rsProf);
-                                                  echo $objProf->nome;
+                                        <td>
+                                            <?php echo("<a href='perfilTurma.php?idTurma=" . $obj->turma_cod . "' > " . $obj->periodo . "/" . $obj->ano . " - " . $obj->turno . " - " . $obj->nome." - " . $obj->modalidade." </a>"); ?>
+                                        </td>
+                                        
+                                        <td>
+                                            <?php
+                                                $codigo = $obj->PROFESSOR_cod;
+                                                $rsCodPessoa = mysql_query("SELECT PESSOA_idPESSOA FROM professor WHERE cod=$codigo");
+                                                $objCodPessoa = mysql_fetch_object($rsCodPessoa);
+                                                $codPessoa = $objCodPessoa->PESSOA_idPESSOA;
+                                                $rsProf = mysql_query("select professor.*, pessoa.* FROM professor inner join pessoa on $codPessoa= pessoa.idPESSOA ORDER BY nome");
+                                                $objProf = mysql_fetch_object($rsProf);
+                                                echo $objProf->nome;
                                             ?>
                                         </td>
+
                                         <td><?php $mes = date('m');
                                                   $ano = date('Y');
                                                   if(($obj->periodo == 1)){
@@ -89,28 +92,26 @@
                                                       }
                                                       }else{echo"nao deu";}
                                                   ?>
-                                        
-                                        
                                         </td>
                                         <td><button type="button" class="btn btn-warning btn-xs">Editar</button> <button type="button" class="btn btn-danger btn-xs" data-toggle="modal" data-target="#modal-delete">Excluir</button></td>
-                                <div class="modal fade" id="modal-delete" tabindex="-1" role="dialog" aria-labelledby="modal-delete" aria-hidden="true">
-                                    <div class="modal-dialog modal-sm">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Sair</span></button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <p>Tem certeza que deseja remover essa turma?</p>
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-default" data-dismiss="modal">Não</button>
-                                                <button type="button" class="btn btn-danger">Sim</button>
+                                        <div class="modal fade" id="modal-delete" tabindex="-1" role="dialog" aria-labelledby="modal-delete" aria-hidden="true">
+                                            <div class="modal-dialog modal-sm">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Sair</span></button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        <p>Tem certeza que deseja remover essa turma?</p>
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-default" data-dismiss="modal">Não</button>
+                                                        <button type="button" class="btn btn-danger">Sim</button>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </div>
-                                </tr>
-                                                <?php }?>
+                                    </tr>
+                                    <?php }?>
                                 </tbody>
                             </table>
                         </div>
