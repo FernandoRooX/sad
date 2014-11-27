d0f50edc4c3909de4c678232ec6a01616795f460
Cleynaldo <cleynaldocarvalho@gmail.com>
2014-11-26 22:14:39 -0200

    editado

--- classes/validaAcesso.php
+++ classes/validaAcesso.php
@@ -9,10 +9,10 @@ $senha = '1010';
 $banco = 'sad9';
 
 #Executa a conexão com o mysql
-$lig = mysql_connect($servidor, $usuario, $senha) or die('Não possível fazer a conexão: ' . mysql_error());
+$lig = @mysql_connect($servidor, $usuario, $senha) or die('Não possível fazer a conexão: ' . mysql_error());
 
 #Seleciona o banco de dados que deseja utilizar
-$select = mysql_select_db($banco);
+$select = @mysql_select_db($banco);
 
 
 $logado = @$_SESSION['logado'];
@@ -41,11 +41,11 @@ if ($logado != true) {
     echo"<script>location.href='../index.php'</script>";
 }
 
-if ($perfil == 'Professor' && ($variavel == "secretaria" || $variavel == "coordenador")) {
+if (@$perfil == 'Professor' && (@$variavel == "secretaria" || @$variavel == "coordenador")) {
     echo"<script>alert('Você não tem permissão para acessar este conteúdo. Acesso negado.')</script>";
     echo"<script>location.href='../professor/index.php'</script>";
 }
-if ($perfil == 'Coordenador' && ($variavel == "secretaria" || $variavel == "professor")) {
+if (@$perfil == 'Coordenador' && (@$variavel == "secretaria" || @$variavel == "professor")) {
     echo"<script>alert('Você não tem permissão para acessar este conteúdo. Acesso negado.')</script>";
     echo"<script>location.href='../coordenador/index.php'</script>";
 }
