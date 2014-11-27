d4692e765ac531bbcca136f4d4f8c77371b4d256
Reuel <reuel_sc@live.com>
2014-11-27 19:08:21 -0200

    acrescentando endereço do arquivo de pesquisa

--- inc/rodape.php
+++ inc/rodape.php
@@ -2,15 +2,12 @@
 	<div class="container container-fluid">
 		<div class="row">
 			<div class="col-md-12">
-<<<<<<< HEAD
-				<p>&copy; Todos os direitos reservados</p>
-=======
 				<p>Sistema de Apoio a Dependência</p>
->>>>>>> 41c89ff57a1e255f6d5c8307caaf5d59d970ab8d
 			</div>
 		</div>
 	</div>
 </footer>
 <script src="../js/jquery.min.js"></script>
 <script src="../js/bootstrap.min.js"></script>
+<script src="../js/jquery-searchable.js"></script>
 <script src="../js/bootstrap-datetimepicker.min.js"></script>
