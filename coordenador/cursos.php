30c70b8ba0104b547e37c3e12eee444e4c75bf9a
Cleynaldo <cleynaldocarvalho@gmail.com>
2014-11-28 14:53:17 -0200

    vinculacao de pagina

--- coordenador/cursos.php
+++ coordenador/cursos.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-    <?php session_start();
+    <?php
+    session_start();
     $variavel = "Coordenador";
     require_once '../classes/validaAcesso.php';
     ?>
     <head>
-        <?php include_once '../inc/head.php'; ?>
+<?php include_once '../inc/head.php'; ?>
 
         <title>Cursos</title>
 
@@ -18,7 +19,7 @@
         <![endif]-->
     </head>
     <body>
-        <?php        require_once '../topo.php';?>
+<?php require_once '../topo.php'; ?>
         <div class="wrapper" role="main">
             <div class="container container-fluid">
                 <div class="row">
@@ -30,13 +31,14 @@
                         <div class="row">
                             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                                 <a href="../coordenador/cadastroCurso.php"><button type="button" class="btn btn-success">Novo Curso</button></a>
+                                <a href="vincularDisciplinaCurso.php"><button type="button" class="btn btn-success">Vincular Disciplina</button></a>
                             </div>
                         </div>
                         <hr>
 
                         <div class="row">
                             <div class="col-lg-4">
-                                <input type="search" id="container-search" class="form-control" placeholder="Pesquisar...">
+                                <input type="search" id="container-search" class="form-control" placeholder="Search...">
                             </div>
                         </div>
 
@@ -52,42 +54,45 @@
                                     </tr>
                                 </thead>
                                 <tbody class="h5">
-                                    <tr>
-                                        <td></td>
-                                        <td></td>
-                                        <td></td>
-                                        <td></td>
-                                        <td><a href="../coordenador/editarCurso.php" type="button" class="btn btn-warning btn-xs">Editar</a> 
-                                            <button type="button" class="btn btn-danger btn-xs" data-toggle="modal" data-target="#modal-delete">Excluir</button></td>
-                                        <div class="modal fade" id="modal-delete" tabindex="-1" role="dialog" aria-labelledby="modal-delete" aria-hidden="true">
-                                            <div class="modal-dialog modal-sm">
-                                                <div class="modal-content">
-                                                    <div class="modal-header">
-                                                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Sair</span></button>
-                                                    </div>
-                                                    <div class="modal-body">
-                                                        <p>Tem certeza que deseja remover esse curso?</p>
-                                                    </div>
-                                                    <div class="modal-footer">
-                                                        <button type="button" class="btn btn-default" data-dismiss="modal">Não</button>
-                                                        <button type="button" class="btn btn-danger">Sim</button>
-                                                    </div>
+                                    <?php
+                                    $rs = mysql_query("select * FROM curso ORDER BY nome");
+                                    while ($obj = mysql_fetch_object($rs)) {
+                                        ?>
+                                        <tr>
+                                            <td><?php echo $obj->nome; ?></td>
+                                            <td><?php echo $obj->orientador; ?></td>
+                                            <td><?php echo $obj->modalidade; ?></td>
+                                            <td><a href="../coordenador/editarCurso.php" type="button" class="btn btn-warning btn-xs">Editar</a> <button type="button" class="btn btn-danger btn-xs" data-toggle="modal" data-target="#modal-delete">Excluir</button></td>
+                                    <div class="modal fade" id="modal-delete" tabindex="-1" role="dialog" aria-labelledby="modal-delete" aria-hidden="true">
+                                        <div class="modal-dialog modal-sm">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Sair</span></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <p>Tem certeza que deseja remover esse curso?</p>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-default" data-dismiss="modal">Não</button>
+                                                    <button type="button" class="btn btn-danger">Sim</button>
                                                 </div>
                                             </div>
                                         </div>
+                                    </div>
                                     </tr>
-                                </tbody>
-                            </table>
+                                    <?php } ?>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>                      
+            </div>                      
+        </div>
     </div>
-</div>
-</div>
-</div>
-<?php include_once '../inc/rodape.php'; ?>
+    </div>
+    </div>
+    <?php include_once '../inc/rodape.php'; ?>
 <script>
     $(function() {
         $('#table').searchable({
